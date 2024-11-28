fe4f27e64a3e61cd28d613f12afea5d8bd69ee15
Michael Rutherford <itsmichaelharmon@gmail.com>
2024-11-28 07:47:49 -0600

    Formatted function brackets

--- src/enemy.cpp
+++ src/enemy.cpp
@@ -19,7 +19,8 @@
 #include "enemy.h"
 
 // Default constructor
-Enemy::Enemy() {
+Enemy::Enemy()
+{
     x = 0;
     y = 0;
     name = "Enemy";
@@ -31,7 +32,8 @@ Enemy::Enemy() {
 }
 
 // Constructor for the Enemy class with specified starting values
-Enemy::Enemy(int start_x, int start_y, std::string start_name, char start_symbol) {
+Enemy::Enemy(int start_x, int start_y, std::string start_name, char start_symbol)
+{
     x = start_x;
     y = start_y;
     name = start_name;
@@ -43,69 +45,85 @@ Enemy::Enemy(int start_x, int start_y, std::string start_name, char start_symbol
 }
 
 // Method to move the enemy to a new position
-void Enemy::moveEnemy(int new_x, int new_y) {
+void Enemy::moveEnemy(int new_x, int new_y)
+{
     x = new_x;
     y = new_y;
 }
 
 // Getters
-int Enemy::getXCoordinate() const {
+int Enemy::getXCoordinate() const
+{
     return x;
 }
 
-int Enemy::getYCoordinate() const {
+int Enemy::getYCoordinate() const
+{
     return y;
 }
 
-std::string Enemy::getName() const {
+std::string Enemy::getName() const
+{
     return name;
 }
 
-int Enemy::getHealth() const {
+int Enemy::getHealth() const
+{
     return health;
 }
 
-int Enemy::getAttack() const {
+int Enemy::getAttack() const
+{
     return attack;
 }
 
-int Enemy::getFOVRadius() const {
+int Enemy::getFOVRadius() const
+{
     return fov_radius;
 }
 
-int Enemy::getColor() const {
+int Enemy::getColor() const
+{
     return color;
 }
 
-char Enemy::getSymbol() const {
+char Enemy::getSymbol() const
+{
     return symbol;
 }
 
 // Setters
-void Enemy::setXCoordinate(int new_x) {
+void Enemy::setXCoordinate(int new_x)
+{
     x = new_x;
 }
 
-void Enemy::setYCoordinate(int new_y) {
+void Enemy::setYCoordinate(int new_y)
+{
     y = new_y;
 }
 
-void Enemy::setHealth(int new_health) {
+void Enemy::setHealth(int new_health)
+{
     health = new_health;
 }
 
-void Enemy::setAttack(int new_attack) {
+void Enemy::setAttack(int new_attack)
+{
     attack = new_attack;
 }
 
-void Enemy::setFOVRadius(int new_radius) {
+void Enemy::setFOVRadius(int new_radius)
+{
     fov_radius = new_radius;
 }
 
-void Enemy::setColor(int new_color) {
+void Enemy::setColor(int new_color)
+{
     color = new_color;
 }
 
-void Enemy::setSymbol(char new_symbol) {
+void Enemy::setSymbol(char new_symbol)
+{
     symbol = new_symbol;
 }
