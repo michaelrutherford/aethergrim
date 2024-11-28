2d300d4489db720284383ae7fd6b6266a6512e8b
Michael Rutherford <itsmichaelharmon@gmail.com>
2024-11-28 08:15:24 -0600

    Formatted function brackets

--- src/item.cpp
+++ src/item.cpp
@@ -19,7 +19,8 @@
 #include "item.h"
 
 // Default constructor
-Item::Item() {
+Item::Item()
+{
     x = 0;
     y = 0;
     name = "Item";
@@ -29,7 +30,8 @@ Item::Item() {
 
 // Parameterized constructor to create an item with specific attributes
 Item::Item(int start_x, int start_y, 
-           std::string new_name, std::string new_description) {
+           std::string new_name, std::string new_description)
+{
     x = start_x;
     y = start_y;
     name = new_name;
@@ -39,51 +41,63 @@ Item::Item(int start_x, int start_y,
 }
 
 // Getters
-int Item::getXCoordinate() const {
+int Item::getXCoordinate() const
+{
     return x;
 }
 
-int Item::getYCoordinate() const {
+int Item::getYCoordinate() const
+{
     return y;
 }
 
-std::string Item::getName() const {
+std::string Item::getName() const
+{
     return name;
 }
 
-std::string Item::getDescription() const {
+std::string Item::getDescription() const
+{
     return description;
 }
 
-int Item::getColor() const {
+int Item::getColor() const
+{
     return color;
 }
 
-char Item::getSymbol() const {
+char Item::getSymbol() const
+{
     return symbol;
 }
 
 // Setters
-void Item::setXCoordinate(int new_x) {
+void Item::setXCoordinate(int new_x)
+{
     x = new_x;
 }
 
-void Item::setYCoordinate(int new_y) {
+void Item::setYCoordinate(int new_y)
+{
     y = new_y;
 }
 
-void Item::setName(std::string new_name) {
+void Item::setName(std::string new_name)
+{
     name = new_name;
 }
 
-void Item::setDescription(std::string new_description) {
+void Item::setDescription(std::string new_description)
+{
     description = new_description;
 }
 
-void Item::setColor(int new_color) {
+void Item::setColor(int new_color)
+{
     color = new_color;
 }
 
-void Item::setSymbol(char new_symbol) {
+void Item::setSymbol(char new_symbol)
+{
     symbol = new_symbol;
 }
