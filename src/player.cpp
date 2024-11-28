59a3914bc7b452aa56f2502d7370de6019e65dde
Michael Rutherford <itsmichaelharmon@gmail.com>
2024-11-28 08:19:43 -0600

    Formatted function brackets

--- src/player.cpp
+++ src/player.cpp
@@ -19,7 +19,8 @@
 #include "player.h"
 
 // Default constructor
-Player::Player() {
+Player::Player()
+{
     x = 0;
     y = 0;
     name = "Player";
@@ -31,68 +32,84 @@ Player::Player() {
 }
 
 // Adds an item to the player's inventory
-void Player::addToInventory(Item new_item) {
+void Player::addToInventory(Item new_item)
+{
     inventory.push_back(new_item);
 }
 
 // Getters
-int Player::getXCoordinate() const {
+int Player::getXCoordinate() const
+{
     return x;
 }
 
-int Player::getYCoordinate() const {
+int Player::getYCoordinate() const
+{
     return y;
 }
 
-std::string Player::getName() const {
+std::string Player::getName() const
+{
     return name;
 }
 
-int Player::getHealth() const {
+int Player::getHealth() const
+{
     return health;
 }
 
-int Player::getAttack() const {
+int Player::getAttack() const
+{
     return attack;
 }
 
-std::vector<Item> Player::getInventory() const {
+std::vector<Item> Player::getInventory() const
+{
     return inventory;
 }
 
-int Player::getColor() const {
+int Player::getColor() const
+{
     return color;
 }
 
-char Player::getSymbol() const {
+char Player::getSymbol() const
+{
     return symbol;
 }
 
 // Setters
-void Player::setXCoordinate(int new_x) {
+void Player::setXCoordinate(int new_x)
+{
     x = new_x;
 }
 
-void Player::setYCoordinate(int new_y) {
+void Player::setYCoordinate(int new_y)
+{
     y = new_y;
 }
 
-void Player::setName(std::string new_name) {
+void Player::setName(std::string new_name)
+{
     name = new_name;
 }
 
-void Player::setHealth(int new_health) {
+void Player::setHealth(int new_health)
+{
     health = new_health;
 }
 
-void Player::setAttack(int new_attack) {
+void Player::setAttack(int new_attack)
+{
     attack = new_attack;
 }
 
-void Player::setColor(int new_color) {
+void Player::setColor(int new_color)
+{
     color = new_color;
 }
 
-void Player::setSymbol(char new_symbol) {
+void Player::setSymbol(char new_symbol)
+{
     symbol = new_symbol;
 }
