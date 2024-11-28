4476821d920512126268274b398e5d8a50f3f6af
Michael Rutherford <itsmichaelharmon@gmail.com>
2024-11-28 08:20:37 -0600

    Formatted function brackets

--- src/tile.cpp
+++ src/tile.cpp
@@ -19,7 +19,8 @@
 #include "tile.h"
 
 // Default constructor
-Tile::Tile() {
+Tile::Tile()
+{
     symbol = ' ';
     color = 0;
     visited = false;
@@ -27,35 +28,43 @@ Tile::Tile() {
 }
 
 // Getters
-char Tile::getSymbol() {
+char Tile::getSymbol()
+{
     return symbol;
 }
 
-int Tile::getColor() {
+int Tile::getColor()
+{
     return color;
 }
 
-bool Tile::getVisited() {
+bool Tile::getVisited()
+{
     return visited;
 }
 
-bool Tile::getInFOV() {
+bool Tile::getInFOV()
+{
     return in_fov;
 }
 
 // Setters
-void Tile::setSymbol(char new_symbol) {
+void Tile::setSymbol(char new_symbol)
+{
     symbol = new_symbol;
 }
 
-void Tile::setColor(int new_color) {
+void Tile::setColor(int new_color)
+{
     color = new_color;
 }
 
-void Tile::setVisited(bool new_status) {
+void Tile::setVisited(bool new_status)
+{
     visited = new_status;
 }
 
-void Tile::setInFOV(bool new_status) {
+void Tile::setInFOV(bool new_status)
+{
     in_fov = new_status;
 }
