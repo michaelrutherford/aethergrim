ddeae8fded936d2f9d106f5772cc3904a7f32150
Michael Rutherford <itsmichaelharmon@gmail.com>
2025-05-24 20:23:11 -0500

    Update item.cpp

--- src/item.cpp
+++ src/item.cpp
@@ -4,85 +4,49 @@
 #include "item.h"
 
 // Default constructor
-Item::Item()
-{
-    x = 0;
-    y = 0;
-    name = "Item";
-    color = 6;
-    symbol = '$';
+Item::Item() {
+  x = 0;
+  y = 0;
+  name = "Item";
+  color = 6;
+  symbol = '!';
 }
 
-// Parameterized constructor to create an item with specific attributes
-Item::Item(int start_x, int start_y, 
-           std::string new_name, std::string new_description)
-{
-    x = start_x;
-    y = start_y;
-    name = new_name;
-    description = new_description;
-    color = 6;
-    symbol = '$';
+// Parameterized constructor
+Item::Item(int start_x, int start_y, std::string new_name,
+           std::string new_description, char new_symbol) {
+  x = start_x;
+  y = start_y;
+  name = new_name;
+  description = new_description;
+  color = 6;
+  symbol = new_symbol;
 }
 
-// Getters
-int Item::getXCoordinate() const
-{
-    return x;
+// Accessor methods
+int Item::getXCoordinate() const { return x; }
+
+int Item::getYCoordinate() const { return y; }
+
+std::string Item::getName() const { return name; }
+
+std::string Item::getDescription() const { return description; }
+
+int Item::getColor() const { return color; }
+
+char Item::getSymbol() const { return symbol; }
+
+// Mutator methods
+void Item::setXCoordinate(int new_x) { x = new_x; }
+
+void Item::setYCoordinate(int new_y) { y = new_y; }
+
+void Item::setName(std::string new_name) { name = new_name; }
+
+void Item::setDescription(std::string new_description) {
+  description = new_description;
 }
 
-int Item::getYCoordinate() const
-{
-    return y;
-}
+void Item::setColor(int new_color) { color = new_color; }
 
-std::string Item::getName() const
-{
-    return name;
-}
-
-std::string Item::getDescription() const
-{
-    return description;
-}
-
-int Item::getColor() const
-{
-    return color;
-}
-
-char Item::getSymbol() const
-{
-    return symbol;
-}
-
-// Setters
-void Item::setXCoordinate(int new_x)
-{
-    x = new_x;
-}
-
-void Item::setYCoordinate(int new_y)
-{
-    y = new_y;
-}
-
-void Item::setName(std::string new_name)
-{
-    name = new_name;
-}
-
-void Item::setDescription(std::string new_description)
-{
-    description = new_description;
-}
-
-void Item::setColor(int new_color)
-{
-    color = new_color;
-}
-
-void Item::setSymbol(char new_symbol)
-{
-    symbol = new_symbol;
-}
+void Item::setSymbol(char new_symbol) { symbol = new_symbol; }
