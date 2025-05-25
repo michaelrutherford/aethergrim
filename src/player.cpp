2d409e60ca9034a35e79106490b60b5cc90c0a65
Michael Rutherford <itsmichaelharmon@gmail.com>
2025-05-24 20:25:31 -0500

    Update player.cpp

--- src/player.cpp
+++ src/player.cpp
@@ -3,98 +3,72 @@
 
 #include "player.h"
 
-// Default constructor
-Player::Player()
-{
-    x = 0;
-    y = 0;
-    name = "Player";
-    health = 100;
-    attack = 10;
-    level = 1;
-    color = 0;
-    symbol = '@';
+// Initialize player attributes and clear inventory
+void Player::initialize() {
+  x = 0;
+  y = 0;
+  name = "Player";
+  health = 30;
+  attack = 8;
+  level = 1;
+  color = 0;
+  symbol = '@';
+  inventory.clear();
 }
 
-// Adds an item to the player's inventory
-void Player::addToInventory(Item new_item)
-{
-    inventory.push_back(new_item);
+// Add an item to the player's inventory
+void Player::addToInventory(Item new_item) { inventory.push_back(new_item); }
+
+// Remove an item from the player's inventory by index
+void Player::removeFromInventory(int index) {
+  inventory.erase(inventory.begin() + index);
 }
 
-// Getters
-int Player::getXCoordinate() const
-{
-    return x;
+// Use an item from the inventory at the given index and remove it
+void Player::useItem(int index) {
+  Item item = getItemAt(index);
+  Log::getInstance().log_command("You used a " + item.getName() + "!");
+
+  inventory.erase(inventory.begin() + index);
+
+  // Heal the player by 10, not exceeding max health of 30
+  if ((health + 10) > 30) {
+    health = 30;
+  } else {
+    health += 10;
+  }
 }
 
-int Player::getYCoordinate() const
-{
-    return y;
-}
+// Accessor methods
+int Player::getXCoordinate() const { return x; }
 
-std::string Player::getName() const
-{
-    return name;
-}
+int Player::getYCoordinate() const { return y; }
 
-int Player::getHealth() const
-{
-    return health;
-}
+std::string Player::getName() const { return name; }
 
-int Player::getAttack() const
-{
-    return attack;
-}
+int Player::getHealth() const { return health; }
 
-std::vector<Item> Player::getInventory() const
-{
-    return inventory;
-}
+int Player::getAttack() const { return attack; }
 
-int Player::getColor() const
-{
-    return color;
-}
+std::vector<Item> Player::getInventory() const { return inventory; }
 
-char Player::getSymbol() const
-{
-    return symbol;
-}
+Item Player::getItemAt(int index) const { return inventory[index]; }
 
-// Setters
-void Player::setXCoordinate(int new_x)
-{
-    x = new_x;
-}
+int Player::getColor() const { return color; }
 
-void Player::setYCoordinate(int new_y)
-{
-    y = new_y;
-}
+char Player::getSymbol() const { return symbol; }
 
-void Player::setName(std::string new_name)
-{
-    name = new_name;
-}
+// Mutator methods
+void Player::setXCoordinate(int new_x) { x = new_x; }
 
-void Player::setHealth(int new_health)
-{
-    health = new_health;
-}
+void Player::setYCoordinate(int new_y) { y = new_y; }
 
-void Player::setAttack(int new_attack)
-{
-    attack = new_attack;
-}
+void Player::setName(std::string new_name) { name = new_name; }
 
-void Player::setColor(int new_color)
-{
-    color = new_color;
-}
+void Player::setHealth(int new_health) { health = new_health; }
 
-void Player::setSymbol(char new_symbol)
-{
-    symbol = new_symbol;
-}
+void Player::setAttack(int new_attack) { attack = new_attack; }
+
+void Player::setColor(int new_color) { color = new_color; }
+
+void Player::setSymbol(char new_symbol) { symbol = new_symbol; }
